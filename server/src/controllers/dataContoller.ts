f53d6a428c5589a4ee58e56dfce5b97a20f24715
meni_levi <1224tap@gmail.com>
2024-12-19 10:02:38 +0200

    Created endPoint from getHighCasualtyArea

--- server/src/controllers/dataContoller.ts
+++ server/src/controllers/dataContoller.ts
@@ -1,5 +1,5 @@
 import express, { IRouter, Request, Response } from "express";
-import { getDeadliestTerrorism } from "../services/dataService";
+import { getDeadliestTerrorism, getHighCasualtyArea } from "../services/dataService";
 import { handleError } from "../../utils/ErrorHandle";
 
 const router: IRouter = express.Router();
@@ -15,5 +15,18 @@ router.get(
     }
   }
 );
+
+router.get(
+  "/api/analysis/highest-casualty-regions/",
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const terrorism = await getHighCasualtyArea();
+      res.json(terrorism);
+    } catch (error: any) {
+      handleError(res, error.status || 404, error.message);
+    }
+  }
+);
+
 
 export default router;
