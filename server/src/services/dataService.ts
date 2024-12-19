eb1d8f6a9fd81516acc6041d5ee7bcf9f3e100b5
meni_levi <1224tap@gmail.com>
2024-12-19 15:17:51 +0200

    Created func to getTerrorOrgByRegions & getTerrorOrgByYear & getDeadliestRegionsByGroup

--- server/src/services/dataService.ts
+++ server/src/services/dataService.ts
@@ -96,4 +96,95 @@ const getIncidentByDate = async (dateToSearch: dateToSearchDTO) => {
   }
 };
 
-export { getDeadliestTerrorism, getHighCasualtyArea, getIncidentByDate };
+
+ const getTerrorOrgByRegions = async (
+  regionName: string,
+  limit: number
+): Promise<ITerror[] | null> => {
+  try {
+    console.log(regionName);
+    return await Terror.aggregate([
+      {
+        $match: { region_txt: regionName },
+      },
+      {
+        $group: {
+          _id: "$gname",
+          total: { $sum: { $sum: ["$nkill", "$nwound"] } },
+        },
+      },
+      {
+        $sort: { total: 1 },
+      },
+      {
+        $limit: limit ? limit : 5,
+      },
+    ]);
+  } catch (error) {
+    throw new Error("Error fetching TerrorEvents");
+  }
+};
+
+ const getTerrorOrgByYear = async (
+  year: number
+): Promise<ITerror[] | null> => {
+  try {
+    return await Terror.aggregate([
+      {
+        $match: { iyear: year },
+      },
+      {
+        $group: {
+          _id: "$gname",
+          total: { $sum: { $sum: ["$nkill", "$nwound"] } },
+        },
+      },
+      {
+        $sort: { total: 1 },
+      },
+    ]);
+  } catch (error) {
+    throw new Error("Error fetching TerrorEvents");
+  }
+};
+
+ const getDeadliestRegionsByGroup = async (nameGroup: string) => {
+  try {
+    const result = await Terror.aggregate([
+      {
+        $match: {
+          gname: nameGroup,
+        },
+      },
+      {
+        $group: {
+          _id: {
+            country: "$country_txt",
+            region: "$region_txt",
+          },
+          total: { $sum: { $add: ["$nkill", "$nwound"] } },
+        },
+      },
+      {
+        $sort: {
+          total: -1,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          region: "$_id.region",
+          country: "$_id.country",
+          total: 1,
+        },
+      },
+    ]);
+
+    return result;
+  } catch (error) {
+    console.error("Error during aggregation:", error);
+    return [];
+  }
+};
+
+export { getDeadliestTerrorism, getHighCasualtyArea, getIncidentByDate,getTerrorOrgByRegions,getTerrorOrgByYear,getDeadliestRegionsByGroup};
