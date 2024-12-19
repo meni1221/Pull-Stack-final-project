d646a9624126e89b3a0df7fe7bbbfa27aadc0bbd
meni_levi <1224tap@gmail.com>
2024-12-19 15:19:02 +0200

    Add Controleres from IncidentByDate & TerrorOrgByRegions & TerrorOrgByYear & DeadliestRegionsByGroup

--- server/src/controllers/dataContoller.ts
+++ server/src/controllers/dataContoller.ts
@@ -1,7 +1,11 @@
 import express, { IRouter, Request, Response } from "express";
 import {
+  getDeadliestRegionsByGroup,
   getDeadliestTerrorism,
   getHighCasualtyArea,
+  getIncidentByDate,
+  getTerrorOrgByRegions,
+  getTerrorOrgByYear,
 } from "../services/dataService";
 import { handleError } from "../../utils/ErrorHandle";
 
@@ -27,16 +31,63 @@ const HighCasualtyArea =
   }
 
 
-// router.get(
-//   "/api/analysis/incident-trends/",
-//   async (req: Request, res: Response): Promise<void> => {
-//     try {
-//       const terrorism = await getIncidentByDate();
-//       res.json(terrorism);
-//     } catch (error: any) {
-//       handleError(res, error.status || 404, error.message);
-//     }
-//   }
-// );
+  const IncidentByDate =  
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const yearStart = Number(req.query.yearStart);
+        const yearEnd = Number(req.query.yearEnd);
+        const monthStart = Number(req.query.monthStart);
+        const monthEnd = Number(req.query.monthEnd);
+        const dateToSearch = { yearStart, yearEnd, monthStart, monthEnd };
+      const terrorism = await getIncidentByDate(dateToSearch);
+      res.json(terrorism);
+    } catch (error: any) {
+      handleError(res, error.status || 404, error.message);
+    }
+  }
 
-export {DeadliestTerrorism,HighCasualtyArea};
+
+   const TerrorOrgByRegions = async (req: Request, res: Response) => {
+    try {
+      const regionName = req.query.regionName as string;
+      const limit = Number(req.query.limit);
+      const TerrorEvents = await getTerrorOrgByRegions(regionName, limit);
+      if (!TerrorEvents) {
+        res.status(404).json({ msg: "Terror Events not found" });
+        return;
+      }
+      res.json(TerrorEvents);
+    } catch (error) {
+      res.status(500).json({ msg: "Server error " + error });
+    }
+  };
+  
+   const TerrorOrgByYear = async (req: Request, res: Response) => {
+    try {
+      const year = Number(req.query.year);
+      const TerrorEvents = await getTerrorOrgByYear(year);
+      if (!TerrorEvents) {
+        res.status(404).json({ msg: "Terror Events not found" });
+        return;
+      }
+      res.json(TerrorEvents);
+    } catch (error) {
+      res.status(500).json({ msg: "Server error " + error });
+    }
+  };
+  
+   const DeadliestRegionsByGroup = async (req: Request, res: Response) => {
+      try {
+        const nameGroup = (req.query.nameGroup as string)
+        const TerrorEvents = await getDeadliestRegionsByGroup(nameGroup);
+        if (!TerrorEvents) {
+          res.status(404).json({ msg: "Terror Events not found" });
+          return;
+        }
+        res.json(TerrorEvents);
+      } catch (error) {
+        res.status(500).json({ msg: "Server error " + error });
+      }
+    };
+
+export {DeadliestTerrorism,HighCasualtyArea,IncidentByDate,TerrorOrgByRegions,TerrorOrgByYear,DeadliestRegionsByGroup};
