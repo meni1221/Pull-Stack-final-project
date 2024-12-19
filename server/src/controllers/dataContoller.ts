9d3f92d7266b704a50b2d66a008d61cd7adffa4b
meni_levi <1224tap@gmail.com>
2024-12-19 11:39:49 +0200

    vriboles

--- server/src/controllers/dataContoller.ts
+++ server/src/controllers/dataContoller.ts
@@ -1,23 +1,22 @@
 import express, { IRouter, Request, Response } from "express";
-import { getDeadliestTerrorism, getHighCasualtyArea } from "../services/dataService";
+import {
+  getDeadliestTerrorism,
+  getHighCasualtyArea,
+} from "../services/dataService";
 import { handleError } from "../../utils/ErrorHandle";
 
 const router: IRouter = express.Router();
 
-router.get(
-  "/api/analysis/deadliest-attack-types/",
-  async (req: Request, res: Response): Promise<void> => {
-    try {
-      const terrorism = await getDeadliestTerrorism();
-      res.json(terrorism);
-    } catch (error: any) {
-      handleError(res, error.status || 404, error.message);
-    }
+const DeadliestTerrorism = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const terrorism = await getDeadliestTerrorism();
+    res.json(terrorism);
+  } catch (error: any) {
+    handleError(res, error.status || 404, error.message);
   }
-);
+};
 
-router.get(
-  "/api/analysis/highest-casualty-regions/",
+const HighCasualtyArea =
   async (req: Request, res: Response): Promise<void> => {
     try {
       const terrorism = await getHighCasualtyArea();
@@ -26,7 +25,18 @@ router.get(
       handleError(res, error.status || 404, error.message);
     }
   }
-);
 
 
-export default router;
+// router.get(
+//   "/api/analysis/incident-trends/",
+//   async (req: Request, res: Response): Promise<void> => {
+//     try {
+//       const terrorism = await getIncidentByDate();
+//       res.json(terrorism);
+//     } catch (error: any) {
+//       handleError(res, error.status || 404, error.message);
+//     }
+//   }
+// );
+
+export {DeadliestTerrorism,HighCasualtyArea};
