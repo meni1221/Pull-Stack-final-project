d36949aac1b7ba308d7ce7a6fa79f0131dae7ef6
meni_levi <1224tap@gmail.com>
2024-12-22 11:32:14 +0200

    Add react dom

--- client/fronted/src/main.tsx
+++ client/fronted/src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "leaflet/dist/leaflet.css"; // <- Leaflet styles
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
