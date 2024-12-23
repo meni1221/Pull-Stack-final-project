9e08189c707694356a3b8bab074214f28366c08f
meni_levi <1224tap@gmail.com>
2024-12-23 18:28:05 +0200

    order on line

--- client/fronted/src/main.tsx
+++ client/fronted/src/main.tsx
@@ -1,20 +1,9 @@
-import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "leaflet/dist/leaflet.css"; // <- Leaflet styles
 import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
