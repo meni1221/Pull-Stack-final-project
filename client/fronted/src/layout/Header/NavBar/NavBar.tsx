bc96ab2ec7cd2c66f73e281106c6d8c6785d184a
meni_levi <1224tap@gmail.com>
2024-12-23 18:29:25 +0200

    created NavBar

---
+++ client/fronted/src/layout/Header/NavBar/NavBar.tsx
@@ -0,0 +1,29 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function NavBar() {
+  return (
+    <Box>
+      <AppBar position="fixed" sx={{ minHeight: "6vh", background: "rgb(67, 90, 171)"}}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
