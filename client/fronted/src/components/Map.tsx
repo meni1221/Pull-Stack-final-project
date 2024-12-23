20b89e53b22eafd79d6a2e12ce7af309a4d8b7cf
meni_levi <1224tap@gmail.com>
2024-12-23 14:55:14 +0200

    orderr on line

--- client/fronted/src/components/Map.tsx
+++ client/fronted/src/components/Map.tsx
@@ -2,12 +2,12 @@ import { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import useFetch from "../hooks/useFetch";
 
-interface  Terror{
-  _id: string
-  region: string
-  count: number,
-  lat: number,
-  long: number,
+interface Terror {
+  _id: string;
+  region: string;
+  count: number;
+  lat: number;
+  long: number;
 }
 const Map = () => {
   const [terrors, setTerrors] = useState<Terror[]>([]);
@@ -34,7 +34,7 @@ const Map = () => {
 
   return (
     <MapContainer
-      center={[32.08556, 34.83639]}
+      center={[34.83639, 32.08556]}
       zoom={5}
       scrollWheelZoom={true}
       style={{ minHeight: "50vh", minWidth: "50vw" }}
@@ -46,7 +46,11 @@ const Map = () => {
       {terrors.map((terror) => (
         <Marker
           key={terror._id}
-          position={terror.lat && terror.long ? [terror.lat as number, terror.long as number] : [ 0, 0]}
+          position={
+            terror.lat && terror.long
+              ? [terror.lat as number, terror.long as number]
+              : [0, 0]
+          }
         >
           <Popup>
             <strong>אזור:</strong> {terror.region} <br />
