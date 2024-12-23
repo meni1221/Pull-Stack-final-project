34f71c975d4f0cbff8a4c4223ef9aef02449ee09
meni_levi <1224tap@gmail.com>
2024-12-23 18:29:40 +0200

    created main

---
+++ client/fronted/src/layout/Main/Main.tsx
@@ -0,0 +1,6 @@
+import {ReactNodeChildrenProp} from "../../interface/ReactNodeChildrenProp"
+export default function Main({children}: ReactNodeChildrenProp) {
+  return (
+    <main>{children}</main>
+  )
+}
