28f68dacfd9c4c549001df065a355e512b539e76
meni_levi <1224tap@gmail.com>
2024-12-23 22:55:06 +0200

    Created DashBoard compo

---
+++ client/fronted/src/components/DashBoard.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { Box, Container } from "@mui/material";
+import ITerror from "../interface/Terror";
+import NoDetailes from "./NoDetailes";
+import { HeaderCard } from "./HeaderCard";
+import DetailsSpace from "./DetailsSpace";
+
+export default function DashBoard() {
+  const BASE_URL = "http://localhost:8181/api/";
+  const [data, setData] = useState<Partial<ITerror[]>>();
+  const [urlToMakeGetData, setUrlToMakeGetData] = useState("");
+
+  return (
+    <Box
+      sx={{
+        marginTop: "5vh",
+      }}
+    >
+      <Container
+        sx={{ display: "flex", flexWrap: "wrap", gap: 2, marginBottom: "20px" }}
+      >
+        <Box sx={{ flex: 1, minWidth: "150px" }}>
+          <HeaderCard
+            setUrlToMakeGetData={setUrlToMakeGetData}
+            setData={setData}
+            urlToMakeGetRequest={BASE_URL + "analysis/deadliest-attack-types"}
+          />
+        </Box>
+        <Box sx={{ flex: 1, minWidth: "150px" }}>
+          <HeaderCard
+            setUrlToMakeGetData={setUrlToMakeGetData}
+            setData={setData}
+            urlToMakeGetRequest={BASE_URL + "analysis/highest-casualty-regions"}
+          />
+        </Box>
+        <Box sx={{ flex: 1, minWidth: "150px" }}>
+          <HeaderCard
+            setUrlToMakeGetData={setUrlToMakeGetData}
+            setData={setData}
+            urlToMakeGetRequest={BASE_URL + "analysis/incident-trends"}
+          />
+        </Box>
+        <Box sx={{ flex: 1, minWidth: "150px" }}>
+          <HeaderCard
+            setUrlToMakeGetData={setUrlToMakeGetData}
+            setData={setData}
+            urlToMakeGetRequest={BASE_URL + "relationships/top-groups"}
+          />
+        </Box>
+        <Box sx={{ flex: 1, minWidth: "150px" }}>
+          <HeaderCard
+            setUrlToMakeGetData={setUrlToMakeGetData}
+            setData={setData}
+            urlToMakeGetRequest={BASE_URL + "relationships/groups-by-year"}
+          />
+        </Box>
+        <Box sx={{ flex: 1, minWidth: "150px" }}>
+          <HeaderCard
+            setUrlToMakeGetData={setUrlToMakeGetData}
+            setData={setData}
+            urlToMakeGetRequest={BASE_URL + "relationships/top-groups"}
+          />
+        </Box>
+      </Container>
+
+      <Box sx={{ marginBottom: "20px" }}>
+        {data ? (
+          <DetailsSpace data={data} urlToMakeGetData={urlToMakeGetData} />
+        ) : (
+          <NoDetailes />
+        )}
+      </Box>
+    </Box>
+  );
+}
