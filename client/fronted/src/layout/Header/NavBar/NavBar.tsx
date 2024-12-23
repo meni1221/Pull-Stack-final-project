f19d7b4d2506aacb0962079440472ae341878834
meni_levi <1224tap@gmail.com>
2024-12-23 22:56:00 +0200

    Edit Typography

--- client/fronted/src/layout/Header/NavBar/NavBar.tsx
+++ client/fronted/src/layout/Header/NavBar/NavBar.tsx
@@ -1,26 +1,18 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 export default function NavBar() {
   return (
     <Box>
-      <AppBar position="fixed" sx={{ minHeight: "6vh", background: "rgb(67, 90, 171)"}}>
+      <AppBar
+        position="fixed"
+        sx={{ minHeight: "6vh", background: "rgb(67, 90, 171)" }}
+      >
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            FINAL PROJECT
           </Typography>
         </Toolbar>
       </AppBar>
