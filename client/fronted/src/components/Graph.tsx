622dd1142f7e23157346d8a3a7b7f4ed66c7873c
meni_levi <1224tap@gmail.com>
2024-12-23 10:31:50 +0200

    add state from terrors

--- client/fronted/src/components/Graph.tsx
+++ client/fronted/src/components/Graph.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
-import { Bar, BarChart, ResponsiveContainer, XAxis } from "recharts";
+import React, { useEffect, useState } from "react";
+import { Bar, BarChart, ResponsiveContainer } from "recharts";
 import useFetch from "../hooks/useFetch";
-
-
+import ITerror from "../interface/Terror";
 
 const Graph: React.FC = () => {
+  const [terrors, setTerrors] = useState<ITerror[]>([]);
   const { data, GET } = useFetch(
     "http://localhost:8181/api/analysis/deadliest-attack-types/"
   );
@@ -13,13 +13,15 @@ const Graph: React.FC = () => {
     GET();
   }, []);
 
+  useEffect(() => {
+    if (data) setTerrors(data);
+    else console.log("not buses");
+  }, [data]);
+
   return (
     <ResponsiveContainer width="100%" height={400}>
-      <BarChart data={data}>
-        <Bar dataKey="uv" fill="green" xAxisId="one" barSize={50} />
-        <XAxis xAxisId="one" />
-        <Bar dataKey="pv" fill="red" xAxisId="two" barSize={30} />
-        <XAxis xAxisId="two" hide />
+      <BarChart data={terrors}>
+        <Bar dataKey="total" fill="red" xAxisId="two" barSize={30} />
       </BarChart>
     </ResponsiveContainer>
   );
