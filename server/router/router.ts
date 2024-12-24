8e98c6eb9838fac608c1e125b2b8bfb597944520
meni_levi <1224tap@gmail.com>
2024-12-24 16:53:14 +0200

    Add Router api/getRegionsName

--- server/router/router.ts
+++ server/router/router.ts
@@ -7,6 +7,7 @@ import {
   TerrorOrgByYear,
   DeadliestRegionsByGroup,
   addTerrorEvent,
+  getRegionsName,
 } from "../src/controllers/dataContoller";
 import { handleError } from "../utils/ErrorHandle";
 
@@ -15,9 +16,10 @@ const router: IRouter = express.Router();
 router.get("/api/analysis/deadliest-attack-types/", DeadliestTerrorism);
 router.get("/api/analysis/highest-casualty-regions/", HighCasualtyArea);
 router.get("/api/analysis/incident-trends/", IncidentByDate);
-router.get("/relationships/top-groups", TerrorOrgByRegions);
+router.get("/api/relationships/top-groups", TerrorOrgByRegions);
 router.get("/relationships/groups-by-year", TerrorOrgByYear);
 router.get("/relationships/deadliest-regions", DeadliestRegionsByGroup);
+router.get("api/getRegionsName", getRegionsName);
 router.post("/addEvent", addTerrorEvent);
 
 router.use((req: Request, res: Response) => {
